477aeb7a33e48acb88dcb5fec85b4524f29d5b8e
truc2004 <nguyenthanhtruc041204@gmail.com>
2025-03-05 22:26:31 +0700

    xong footer

--- travel-app/src/App.jsx
+++ travel-app/src/App.jsx
@@ -3,17 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Login from "./pages/Login";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
+// import Footer from "./components/Footer";
 
 function App() {
   console.log("chay")
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
